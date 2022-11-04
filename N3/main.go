935aa6d037a58714c72489ee464478ad5a6098dd
Anton Bortnikov <toha1100@gmail.com>
2022-11-04 21:22:20 +0500

    add lesson 3

---
+++ N3/main.go
@@ -0,0 +1,52 @@
+package n3
+
+import (
+	"sort"
+	"strings"
+	"unicode"
+)
+
+type WordOccurence struct {
+	word  string
+	count uint
+}
+
+func Top10(s string) []string {
+	words := strings.FieldsFunc(s, func(r rune) bool {
+		return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+	})
+	countsMap := make(map[string]uint, len(words))
+
+	for _, word := range words {
+		lowered_word := strings.ToLower(word)
+		countsMap[lowered_word]++
+	}
+
+	counts := make([]WordOccurence, 0, len(countsMap))
+	for word, count := range countsMap {
+		counts = append(counts, WordOccurence{word, count})
+	}
+
+	sort.Slice(counts, func(i int, j int) bool {
+		a, b := counts[i], counts[j]
+		if a.count == b.count {
+			return a.word < b.word
+		}
+		return a.count > b.count
+	})
+
+	top_cap := 10
+	if top_cap > len(counts) {
+		top_cap = len(counts)
+	}
+
+	top10 := counts[:top_cap]
+
+	top10strings := make([]string, 0, top_cap)
+
+	for _, word_occurence := range top10 {
+		top10strings = append(top10strings, word_occurence.word)
+	}
+
+	return top10strings
+}
---
+++ N3/main_test.go
@@ -0,0 +1,25 @@
+package n3
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestWithAtLeast10Words(t *testing.T) {
+	require.Equal(t, []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}, Top10("0 1 2 3 4 5 6 7 8 9"))
+	require.Equal(t, []string{"0", "1", "10", "2", "3", "4", "5", "6", "7", "8"}, Top10("10 0 1 2 3 4 5 6 7 8 9"))
+	require.Equal(t, []string{"the", "a", "and", "dummy", "industry", "ipsum", "lorem", "of", "text", "type"}, Top10("Lorem Ipsum is simply dummy, text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."))
+}
+
+func TestWithAtLessThan10Words(t *testing.T) {
+	require.Equal(t, []string{"0", "1", "2", "3", "4"}, Top10("0 1 2 3 4"))
+
+	require.Equal(t, []string{"1", "3"}, Top10("3 1 1 3 1"))
+
+	require.Equal(t, []string{}, Top10(""))
+}
+
+func TestCaseInsencive(t *testing.T) {
+	require.Equal(t, []string{"юникод", "test"}, Top10("test Test юникод ЮнИкОд ЮНИКОД"))
+}
